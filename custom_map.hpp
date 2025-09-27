be7010cfda943077c018dac0f0dec4622dc80061
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2025-09-27 00:16:58 -0400

    use murmurhash, not much diff seen

--- custom_map.hpp
+++ custom_map.hpp
@@ -2,13 +2,55 @@
 #define CUSTOM_MAP
 
 #include <cstdint>
+#include <cstring>
 #include <functional>
 
 template <typename K, typename V> struct Slot {
-    size_t fingerprint = 0;
+    uint32_t fingerprint = 0;
     K key;
     V value;
 };
+
+static inline uint32_t murmur_32_scramble(uint32_t k) {
+    k *= 0xcc9e2d51;
+    k = (k << 15) | (k >> 17);
+    k *= 0x1b873593;
+    return k;
+}
+
+uint32_t murmur3_32(const uint8_t* key, size_t len, uint32_t seed)
+{
+	uint32_t h = seed;
+    uint32_t k;
+    /* Read in groups of 4. */
+    for (size_t i = len >> 2; i; i--) {
+        // Here is a source of differing results across endiannesses.
+        // A swap here has no effects on hash properties though.
+        memcpy(&k, key, sizeof(uint32_t));
+        key += sizeof(uint32_t);
+        h ^= murmur_32_scramble(k);
+        h = (h << 13) | (h >> 19);
+        h = h * 5 + 0xe6546b64;
+    }
+    /* Read the rest. */
+    k = 0;
+    for (size_t i = len & 3; i; i--) {
+        k <<= 8;
+        k |= key[i - 1];
+    }
+    // A swap is *not* necessary here because the preceding loop already
+    // places the low bytes in the low places according to whatever endianness
+    // we use. Swaps only apply when the memory is copied in a chunk.
+    h ^= murmur_32_scramble(k);
+    /* Finalize. */
+	h ^= len;
+	h ^= h >> 16;
+	h *= 0x85ebca6b;
+	h ^= h >> 13;
+	h *= 0xc2b2ae35;
+	h ^= h >> 16;
+	return h;
+}
 
 // nearest power of two helper
 static inline constexpr size_t nearest_power_of_two(size_t number) {
@@ -39,9 +81,9 @@ template <typename K, typename V, const size_t __min_slots = 1> class MyMap {
 
   public:
     __attribute__((noinline)) void insert(const K &key, V value) noexcept {
-        size_t hash = std::hash<K>{}(key);
-        size_t fingerprint = hash; // cheap tag
-        fingerprint &= ~static_cast<uint16_t>(1);
+        uint32_t hash = murmur3_32((const uint8_t*) key.data(), key.length(), 42);
+        uint32_t fingerprint = hash; // cheap tag
+        fingerprint &= ~static_cast<uint32_t>(1);
 
         size_t bucket = hash & (capacity() - 1);
         while (true) {
@@ -55,11 +97,10 @@ template <typename K, typename V, const size_t __min_slots = 1> class MyMap {
             }
 
             if (slot.fingerprint == (fingerprint | 1) && slot.key == key) {
-                slot.value = value; // update existing
+                slot.value = value;
                 return;
             }
 
-            // linear probe
             bucket = (bucket + 1) & (capacity() - 1);
         }
     }
