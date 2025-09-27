ae203192ad9e58cffc773dd83194fc919eb0545b
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2025-09-26 23:33:08 -0400

    [start here], shorter code with robinhood hashing

--- custom_map.hpp
+++ custom_map.hpp
@@ -2,7 +2,6 @@
 #define CUSTOM_MAP
 
 #include <functional>
-#include <stdexcept>
 
 template <typename K, typename V> struct Slot {
     size_t hash = 0;
@@ -21,7 +20,6 @@ static inline constexpr size_t nearest_power_of_two(size_t number) {
     number |= number >> 16;
     number |= number >> 32;
     number++;
-
     return number;
 }
 
@@ -34,41 +32,35 @@ template <typename K, typename V, const size_t __min_slots = 1> class MyMap {
     }
 
   public:
-    inline void insert(const K &key, V value) {
-        size_t hash = std::hash<K>{}(key);
-        size_t bucket = hash & (capacity() - 1);
-        size_t vpsl = 0;
+    inline void insert(const K &key, V value) noexcept {
+        Slot<K, V> to_insert;
+        to_insert.hash = std::hash<K>{}(key);
+        to_insert.key = key;
+        to_insert.value = value;
+        to_insert.psl = 0;
+        to_insert.occupied = true;
 
-        K current_key = key;
-        V current_value = value;
+        size_t bucket = to_insert.hash & (capacity() - 1);
 
         while (true) {
             if (!slots[bucket].occupied) {
-                slots[bucket].occupied = true;
-                slots[bucket].key = key;
-                slots[bucket].value = value;
-                slots[bucket].psl = vpsl;
-                slots[bucket].hash = hash;
+                slots[bucket] = to_insert;
                 return;
             }
 
-            if (slots[bucket].hash == hash && slots[bucket].key == key) {
+            if (slots[bucket].hash == to_insert.hash &&
+                slots[bucket].key == to_insert.key) {
                 slots[bucket].value = value;
                 return;
             }
 
-            if (vpsl > slots[bucket].psl) {
-                std::swap(current_key, slots[bucket].key);
-                std::swap(current_value, slots[bucket].value);
-                std::swap(vpsl, slots[bucket].psl);
-                std::swap(hash, slots[bucket].hash);
+            if (to_insert.psl > slots[bucket].psl) {
+                std::swap(to_insert, slots[bucket]);
             }
 
-            vpsl += 1;
+            to_insert.psl++;
             bucket = (bucket + 1) & (capacity() - 1);
         }
-
-        throw std::runtime_error("Hashmap is full");
     }
 };
 
